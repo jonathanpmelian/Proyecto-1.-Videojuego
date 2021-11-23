47fb494e53cd8d0f8e21490a9626ea0fe9da609f
= <adrian.diezg@hotmail.com>
2021-11-23 22:20:05 +0100

    started refactoring

--- scr/obstacle.js
+++ scr/obstacle.js
@@ -1,9 +1,9 @@
 function Obstacle() {
-    $obstacle: null,
-    lane: null,
-    currentPos: { y: 100 },
-    dimensions: { w: 11.3 , h: 5.62 },
-    createObstacle() {
+    this.$obstacle = null
+    this.lane = null,
+    this. yPos = 100
+    this.dimensions = { w: 11.3 , h: 5.62 }
+    this.createObstacle = function() {
         this.$obstacle = document.createElement('div')
         let road = document.getElementById('road')
         road.appendChild(this.$obstacle)
@@ -12,17 +12,17 @@ function Obstacle() {
             let lanes = [10,33,55,78]
             let randomLane = lanes[Math.floor(Math.random()*4)]
             return randomLane
-        }();
+        }.bind(Obstacle)();
         this.$obstacle.style.left = `${this.lane}%`
-        this.$obstacle.style.bottom = `${this.currentPos.y}%`
-        this.$obstacle.style.width = `${this.dimensions.w}%`
-        this.$obstacle.style.height = `${this.dimensions.h}%`
+        this.$obstacle.style.bottom = `${this.yPos}px`
+        this.$obstacle.style.width = `${this.dimensions.w}px`
+        this.$obstacle.style.height = `${this.dimensions.h}px`
     },
-    movement() {
+    this.movement = function() {
         let timerObstacle = setInterval(function() {
-            this.currentPos.y -= canvas.roadSpeed/1.7; //En =1.7 fijo, >1.7 mismo sentido, <1.7 sentido contrario
-            this.$obstacle.style.bottom = `${this.currentPos.y}%`
-            if(this.currentPos.y < -20) { clearInterval(timerObstacle) }
-        }.bind(obstacle),10)
+            this.yPos -= canvas.roadSpeed/1.7; //En =1.7 fijo, >1.7 mismo sentido, <1.7 sentido contrario
+            this.$obstacle.style.bottom = `${this.yPos}px`
+            if(this.yPos < -20) { clearInterval(timerObstacle) }
+        }.bind(Obstacle),10)
     }
 }
